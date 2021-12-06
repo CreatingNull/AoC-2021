68959c3ef077aa7b3ad146ed7b8c4bab054d7e99
nulltek <steve.public@nulltek.xyz>
2021-12-06 19:20:04 +1300

    Adding day 6 solution, data and tests.

---
+++ src/day6/__init__.py
@@ -0,0 +1,34 @@
+"""--- Day 6: Lanternfish ---"""
+from pathlib import Path
+
+
+def simulate_population(fish: {}, days: int) -> {}:
+    """Step through the days adding additional '8' keyed fish from the '0'
+    keyed parents.
+
+    :param fish: Dictionary containing day counter keys to number of fish values.
+    :param days: Number of days to simulate.
+    :return: Dictionary containing the updated state of the fish.
+    """
+    for day in range(days):
+        daily_fish = fish[0]
+        for index in range(0, 8):
+            if index in fish:  # shift the fish 8 -> 1 down
+                fish[index] = fish[index + 1]
+        fish[8] = daily_fish  # add new fish
+        fish[6] += daily_fish  # reset parent fish
+    return fish
+
+
+def load_dataset(dataset_path: Path) -> {}:
+    """Loads the fishy fish from file.
+
+    :param dataset_path: Path object to load the data from.
+    :return: Dictionary containing day counter keys to number of fish values.
+    """
+    data = {i: 0 for i in range(9)}  # The possible counter states for the fish
+    with open(dataset_path) as file:
+        for line in file:
+            for digit in line.split(","):
+                data[int(digit)] += 1
+    return data
---
+++ src/tests/test_day6.py
@@ -0,0 +1,33 @@
+"""Test cases for day 6."""
+from pathlib import Path
+
+import pytest
+from aoc import log
+from aoc import ROOT_PATH
+from day6 import load_dataset
+from day6 import simulate_population
+
+
+@pytest.mark.parametrize(
+    "dataset_path,days,result",
+    [
+        [Path("day6/data/data-small.txt"), 80, 5934],
+        [Path("day6/data/data-large.txt"), 80, 346063],
+        [Path("day6/data/data-small.txt"), 256, 26984457539],
+        [Path("day6/data/data-large.txt"), 256, 1572358335990],
+    ],
+)
+def test_day(dataset_path: Path, days: int, result: int):
+    """Test case for verifying the results of day 6."""
+    data = load_dataset(ROOT_PATH.joinpath(dataset_path))
+    log.info(
+        "Loaded day 6 dataset %s with %d fish.", dataset_path.name, sum(data.values())
+    )
+    data = simulate_population(data, days)
+    total_fish = sum(data.values())
+    log.info(
+        "after %d days there are %d total fish.",
+        days,
+        total_fish,
+    )
+    assert result == total_fish
